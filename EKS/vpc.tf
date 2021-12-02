0b2057b3d784ee6f5ea6c68d61101a233c00d7b8
mkaursandhu <80225229+mkaursandhu@users.noreply.github.com>
2021-12-02 12:07:44 -0500

    Update vpc.tf

--- EKS/vpc.tf
+++ EKS/vpc.tf
@@ -11,7 +11,7 @@ resource "aws_vpc" "demo" {
 
   tags = map(
     "Name", "terraform-eks-demo-node",
-    "kubernetes.io/cluster/${var.cluster-name}", "shared",
+    "kubernetes.io/cluster/${var.cluster-name}", "shared"
   )
 }
 
@@ -25,7 +25,7 @@ resource "aws_subnet" "demo" {
 
   tags = map(
     "Name", "terraform-eks-demo-node",
-    "kubernetes.io/cluster/${var.cluster-name}", "shared",
+    "kubernetes.io/cluster/${var.cluster-name}", "shared"
   )
 }
 
